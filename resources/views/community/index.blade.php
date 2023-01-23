4eb2c480621d45fd9a1144518f044022b66b3f88
JSadracAB <jsdracab@gmail.com>
2023-01-23 16:37:41 +0000

    Actividad A32

---
+++ resources/views/community/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+
+<h1>Community</h1>
+
+<!-- Link table -->
+@foreach ($links as $link)
+<li>{{$link->title}} <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small></li>
+
+@endforeach
+{{$links->links()}}
+
+@stop
