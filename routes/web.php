9832bff62d6199a645898db3c13ef5d496519b68
Joel <you@example.com>
2023-02-01 19:20:09 +0000

    clase

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\CommunityLinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +21,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('community', [CommunityLinkController::class, 'index']);
-
-Route::post('community', [CommunityLinkController::class, 'store']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
