6446f77e9af70252931515995988ad086f7e82af
JSadracAB <jsdracab@gmail.com>
2023-02-24 15:52:08 +0000

    Tarea A40

--- resources/views/community/partials/list-links.blade.php
+++ resources/views/community/partials/list-links.blade.php
@@ -15,16 +15,24 @@
     <ul>
         @foreach ($links as $link)
         <li class="row">
-            <div class="link col-8">
-                <p>
-                    <a href="{{$link->link}}" target="_blank">
-                        {{$link->title}}
-                    </a>
-                </p>
-                <a href="/community/{{ $link->channel->slug }}" style="color: {{ $link->channel->color }}">{{$link->channel->slug}}</a>
-                <small>Contributed by: <b class="creator">{{$link->creator->name}}</b> {{$link->updated_at->diffForHumans()}}</small>
+
+            <div class="likes px-3 col-1">
+                <img src="{{asset('images/like.png')}}" alt="Likes">
+                <p class="ml-1">{{$link->users()->count()}}</p>
             </div>
-            <div class="channel col-3">
+
+            <div class="links col-9">
+                <div>
+                    <p>
+                        <a href="{{$link->link}}" target="_blank">
+                            {{$link->title}}
+                        </a>
+                    </p>
+                    <a href="/community/{{ $link->channel->slug }}" style="color: {{ $link->channel->color }}">{{$link->channel->slug}}</a>
+                    <small>Contributed by: <b class="creator">{{$link->creator->name}}</b> {{$link->updated_at->diffForHumans()}}</small>
+                </div>
+            </div>
+            <div class="channels col-2">
                 <span class="label label-default" style="background: {{ $link->channel->color }}">
                     {{ strtoupper($link->channel->title) }}
                 </span>
