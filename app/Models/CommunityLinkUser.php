292aef58a72b992eb1eea5e73ca40eb87b2c16c4
JSadracAB <jsdracab@gmail.com>
2023-02-25 19:30:45 +0000

    Comentario

--- app/Models/CommunityLinkUser.php
+++ app/Models/CommunityLinkUser.php
@@ -17,13 +17,17 @@ class CommunityLinkUser extends Model
 
     public function toggle($link)
     {
+        // Busca el primer modelo que coincida con las restricciones
         $vote = $this->firstOrNew([
             'user_id' => Auth::id(),
             'community_link_id' => $link->id
         ]);
 
+        // Si existe, lo borra (1 voto menos)
         if ($vote->id) {
             $vote->delete();
+
+        // Si no, lo crea (1 voto más)
         } else {
             $vote->save();
         }
